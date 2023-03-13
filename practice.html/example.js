0447414f534c062f6faf9f1d93412c0ac50955a1
eniyongabo Emmanuel <emmanuelniyongabo@Emmanuels-MacBook-Pro.local>
2023-03-13 14:47:17 -0500

    Lab 9 files updates

---
+++ practice.html/example.js
@@ -0,0 +1,14 @@
+
+
+$(document).ready(function(){
+$("#pappy").click(function(){
+    $("#imgPappy").show();
+    $("#imgKetty").hide();
+});
+
+$("#kitty").click(function(){
+    $("#imgKitty").show();
+    $("#imgPappy".hide());
+});
+
+});
